c976c230d5e432022a3504f4b3c1fd9ab2744b3b
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-19 12:26:09 -0800

    checking pawns pieces

---
+++ ChessDictionaryValidator.py
@@ -0,0 +1,32 @@
+#Chess Dictionary Validator
+board = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e': 'wking'}
+
+def isValidChessBoard():
+
+
+    pieces = ['king', 'queen', 'rook', 'bishop', 'knight', 'pawn']
+    colors = ['w', 'b']
+    y = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    x = ['1', '2', '3', '4', '5', '6', '7', '8']
+    bpieces, wpieces = 0, 0
+    bking, wking, bpawn, wpawn = 0, 0, 0, 0
+
+    #returns true or false if board if valid
+    #one black king and one white king
+    if bking and wking != 1:
+        return False
+    
+    #each player can have at most 16 pieces
+    if bpieces or wpieces > 16:
+        return False
+    #no more than 8 pawns
+    if bpawn or wpawn > 8:
+        return False
+    #all pieces must be on a valid space from '1a' to '8h'
+    for pos in board.keys():
+        if pos[0] not in x or pos[1] not in y:
+            return False
+            
+    #pieces begin with either 'w' or 'b' followed by 'pawn', 'knight', 'bishop', 'rook', 'queen', or 'king'
+    #should detect when a bug has resulted in an improper chess board state
+print(isValidChessBoard())
