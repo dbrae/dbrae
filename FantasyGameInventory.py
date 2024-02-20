97caf88e475c54c27885d927d2ee8a027bc96653
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-19 18:15:45 -0800

    added loot pickup

--- FantasyGameInventory.py
+++ FantasyGameInventory.py
@@ -1,5 +1,3 @@
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
 def displayInventory(inventory):
     print('Inventory:')
     item_total = 0
@@ -8,4 +6,13 @@ def displayInventory(inventory):
         item_total += v
     print('Total number of items: ' + str(item_total))
 
-displayInventory(stuff)
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item, 0)
+        inventory[item] += 1
+    return inventory
+
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
