a74c9d5d37cfa72713be1a9308b7b0affe848530
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-22 14:49:35 -0800

    Paste text from the clipboard

---
+++ bulletPointAdder.py
@@ -0,0 +1,13 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# TODO: Separate lines and add stars.
+
+
+#paste text from the clipboard
+#do something to it
+#copy the new text to the clipboard
