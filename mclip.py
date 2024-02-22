4c1b39a1e864e68eff5ca02bbef14b4b4e414d7d
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-22 14:17:33 -0800

    add batch file

--- mclip.py
+++ mclip.py
@@ -17,3 +17,11 @@ if keyphrase in TEXT:
     print('Text for ' + keyphrase + ' copied to clipboard.')
 else:
     print('There is no text for ' + keyphrase)
+
+# To create a batch file to run the program, create a new file with the following content and save it with a .bat file extension:
+# @py.exe C:\path\to\mclip.py %*
+# @pause
+# If you don’t include the @pause line, the command line window will close as soon as the program finishes running, and you won’t be able to see the output.
+# You can also run the program from the command line by typing mclip.py and the keyphrase.
+# For example, to copy the upsell text to the clipboard, you would type the following command into the command line:
+# mclip.py upsell
