4749d64fd7567f3e3fb56f618bd015f3a9928b6e
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-22 16:52:12 -0800

    seperate lines and add star

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -5,9 +5,12 @@
 import pyperclip
 text = pyperclip.paste()
 
-# TODO: Separate lines and add stars.
+# Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)): # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
 
-
+pyperclip.copy(text)
 #paste text from the clipboard
 #do something to it
 #copy the new text to the clipboard
