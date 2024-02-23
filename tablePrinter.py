d489ae031da28415db867c0afcb1c15cc65c0f39
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-23 15:00:57 -0800

    added comments

---
+++ tablePrinter.py
@@ -0,0 +1,17 @@
+#! python3
+#printTable()
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+            ['Alice', 'Bob', 'Carol', 'David'],
+            ['dogs', 'cats', 'moose', 'goose']]
+def printTable(tableData):
+    colWidths = [0] * len(tableData)
+    for i in range(len(tableData)): # find the number of lists in tableData
+        for j in range(len(tableData[i])): # find the longest string in each list of tableData
+            if colWidths[i] < len(tableData[i][j]): # if the length of the current string is greater than the current value of colWidths[i]
+                colWidths[i] = len(tableData[i][j]) # set the value of colWidths[i] to the length of the current string
+    for i in range(len(tableData[0])):
+        for j in range(len(tableData)):
+            print(tableData[j][i].rjust(colWidths[j]), end=' ') # rjust() method of string right-justifies the string in a field of a given width by padding it with spaces on the left
+        print()
+
+printTable(tableData)
