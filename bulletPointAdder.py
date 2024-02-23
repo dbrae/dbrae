6d153c35be91f23bca9e19b3e241de4a8f12634c
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-22 21:42:38 -0800

    Join modified lines

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -10,6 +10,8 @@ lines = text.split('\n')
 for i in range(len(lines)): # loop through all indexes in the "lines" list
     lines[i] = '* ' + lines[i] # add star to each string in "lines" list
 
+text = '\n'.join(lines)
+
 pyperclip.copy(text)
 #paste text from the clipboard
 #do something to it
