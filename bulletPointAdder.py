8c52cfb9779a503f7ae8169ea7d48718390b6017
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-22 21:45:01 -0800

    copy the new text to the clipboard

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -13,6 +13,4 @@ for i in range(len(lines)): # loop through all indexes in the "lines" list
 text = '\n'.join(lines)
 
 pyperclip.copy(text)
-#paste text from the clipboard
-#do something to it
 #copy the new text to the clipboard
