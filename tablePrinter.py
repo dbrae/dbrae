59a2dc2e68b936f8f93775f2bbb2118c21bd6234
dbrae <124220893+dbrae@users.noreply.github.com>
2024-02-23 15:01:32 -0800

    more comments

--- tablePrinter.py
+++ tablePrinter.py
@@ -9,8 +9,8 @@ def printTable(tableData):
         for j in range(len(tableData[i])): # find the longest string in each list of tableData
             if colWidths[i] < len(tableData[i][j]): # if the length of the current string is greater than the current value of colWidths[i]
                 colWidths[i] = len(tableData[i][j]) # set the value of colWidths[i] to the length of the current string
-    for i in range(len(tableData[0])):
-        for j in range(len(tableData)):
+    for i in range(len(tableData[0])):  # find the number of strings in each list of tableData
+        for j in range(len(tableData)): # find the number of lists in tableData
             print(tableData[j][i].rjust(colWidths[j]), end=' ') # rjust() method of string right-justifies the string in a field of a given width by padding it with spaces on the left
         print()
 
