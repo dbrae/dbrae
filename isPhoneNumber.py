3d38630356ffb366c6f17e11d01fd47e4bdc6815
dbrae <124220893+dbrae@users.noreply.github.com>
2024-03-18 13:33:46 -0700

    fixed error with text written as tesxt

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -1,4 +1,4 @@
-def isPhoneNumber(tesxt):
+def isPhoneNumber(text):
     if len(text) != 12:
         return False
     for i in range(0,3):
@@ -16,7 +16,9 @@ def isPhoneNumber(tesxt):
             return False
     return True
 
-print('415-555-4242 is a phone number:')
-print(isPhoneNumber('415-555-4242'))
-print('Moshi moshi is a phone number:')
-print(isPhoneNumber('Moshi moshi'))
+message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found: ' + chunk)
+print('Done')
