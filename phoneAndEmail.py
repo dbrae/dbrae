a8bac6264b1adf16f99c342e72aba4e112134327
dbrae <124220893+dbrae@users.noreply.github.com>
2024-04-14 18:51:08 -0700

    options

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -39,3 +39,10 @@ if len(matches) > 0:
     print('\n'.join(matches))
 else:
     print('No phone numbers or email addresses found.')
+
+#For example, you could:
+#
+#Find website URLs that begin with http:// or https://.
+#Clean up dates in different date formats (such as 3/14/2019, 03-14-2019, and 2015/3/19) by replacing them with dates in a single, standard format.
+#Remove sensitive information such as Social Security or credit card numbers.
+#Find common typos such as multiple spaces between words, accidentally accidentally repeated words, or multiple exclamation marks at the end of sentences. Those are annoying!!
