c463bb59e8bb5e4c5a0ddb8fc0f6e93ffe9c777d
dbrae <124220893+dbrae@users.noreply.github.com>
2024-05-13 20:54:07 -0700

    English version

--- idiot.py
+++ idiot.py
@@ -1,3 +1,14 @@
 #Ask the user if they'd like to know how to keep an idiot busy for hours.
 #If the user answers 'no', quit.
 #If the user answers 'yes', go to step 1.
+
+import pyinputplus as pyip
+
+while True:
+    promt = 'Want to know how to keep an idiot busy for hours?\n'
+    response = pyip.inputYesNo(promt)
+    if response == 'no':
+        break
+
+print('Thank you. Have a nice day.')
+
