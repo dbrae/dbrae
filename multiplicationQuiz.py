00e23669a61799b3f019bd2be9455e1f8f0edc5c
dbrae <124220893+dbrae@users.noreply.github.com>
2024-05-20 20:38:25 -0700

    try right and wrong answers

--- multiplicationQuiz.py
+++ multiplicationQuiz.py
@@ -4,3 +4,15 @@ import random, time
 numberOfQuestions = 10
 correctAnswers = 0
 for questionNumber in range(numberOfQuestions):
+    # Pick two random numbers:
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+
+    prompt = '#%s: %s x %s = ' % (questionNumber, num1, num2)
+
+    try:
+        # Right answers are handled by allowRegexes.
+        # Wrong answers are handled by blockRegexes, with a custom message.
+        pyip.inputStr(prompt, allowRegexes=['^%s$' % (num1 * num2)],
+                      blockRegexes=[('.*', 'Incorrect!')],
+                      timeout=8, limit=3)
