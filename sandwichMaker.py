68706eacda1ecc4aaa38b883436db6056532ab9e
dbrae <124220893+dbrae@users.noreply.github.com>
2024-05-20 20:46:53 -0700

    Added rules

---
+++ sandwichMaker.py
@@ -0,0 +1,8 @@
+#using inputMenu() for a bread type: wheat, white, sourdough
+#using inputMenu() for a protein type: chicken, turkey, ham, tofu
+#using inputYesNo() to ask if they want cheese
+#if so, using inputMenu() to pick a cheese type: cheddar, Swiss, mozzarella
+#using inputYesNo() to ask if they want mayo, mustard, lettuce, or tomato
+#using inputInt() to ask how many sandwiches they want. Make sure this number is 1 or more.
+
+#come up with prices for each of these options, and have your program display a total cost after the user enters their selection.
