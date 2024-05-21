7d4cdc52f0c49834abf9e1f1731f1bb6e10b77e1
dbrae <124220893+dbrae@users.noreply.github.com>
2024-05-20 20:51:46 -0700

    Requirements for my own multiplication quiz

---
+++ myMultiplicationQuiz.py
@@ -0,0 +1,5 @@
+#prompt user with 10 multiplication questions, ranging from 0 x 0 to 9 x 9.
+#If the user answers correctly, display "Correct!" for 1 second and move on to the next question.
+#The user gets three tries to answer each question before the program moves on to the next question.
+#Eight seconds after first displaying the question, the question is marked as incorrect even if the user enters the correct answer after the 8-second limit.
+
