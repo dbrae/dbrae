28bb8b09e0ea43be37b58ef5c1bfefe3515b8532
dbrae <124220893+dbrae@users.noreply.github.com>
2024-05-20 21:31:05 -0700

    added bread, protein, cheese

--- sandwichMaker.py
+++ sandwichMaker.py
@@ -4,9 +4,18 @@ import pyinputplus as pyip
 bread = pyip.inputMenu(['wheat', 'white', 'sourdough'], numbered=True)
 
 #using inputMenu() for a protein type: chicken, turkey, ham, tofu
+protein = pyip.inputMenu(['chicken', 'turkey', 'ham', 'tofu'], numbered=True)
+
 #using inputYesNo() to ask if they want cheese
+cheese = pyip.inputYesNo('Do you want cheese?')
+
 #if so, using inputMenu() to pick a cheese type: cheddar, Swiss, mozzarella
+if cheese == 'yes':
+    cheeseType = pyip.inputMenu(['cheddar', 'Swiss', 'mozzarella'], numbered=True)
+
 #using inputYesNo() to ask if they want mayo, mustard, lettuce, or tomato
+mayo = pyip.inputYesNo('Do you want mayo?')
+
 #using inputInt() to ask how many sandwiches they want. Make sure this number is 1 or more.
 
 #come up with prices for each of these options, and have your program display a total cost after the user enters their selection.
